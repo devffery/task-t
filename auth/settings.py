f14a9857e294c1b15ae00d85eaab491a3bae4e89
Samuel <168239593+devffery@users.noreply.github.com>
2024-07-07 17:08:15 +0100

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [task-t.onrender.com]
+ALLOWED_HOSTS = ['task-t.onrender.com', 'vercel.app']
 
 
 # Application definition
