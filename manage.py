f990e68904880539aeff51e157f7620224eebd13
Samuel <168239593+devffery@users.noreply.github.com>
2024-07-07 18:28:29 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,7 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    # Specify the port here, adjust as needed
+    port = int(os.environ.get("PORT", 8000))
+    execute_from_command_line(sys.argv, addrport=f"0.0.0.0:{port}")
 
 
 if __name__ == '__main__':
